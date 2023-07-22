a77fdbee5df97906f95b44b199acb2546626ce18
Lendly Cagata <lendly.cagata@telus.com>
2023-07-23 04:34:20 +0800

    chore: comment instance

--- terraform/mysql.tf
+++ terraform/mysql.tf
@@ -1,26 +1,26 @@
-resource "google_sql_database_instance" "instance" {
+# resource "google_sql_database_instance" "instance" {
 
-  name             = "my-instance"
-  database_version = "MYSQL_5_7"
-  region           = "northamerica-northeast1"
-  settings {
-    tier = "db-f1-micro"
-  }
-  deletion_protection = false
+#   name             = "my-instance"
+#   database_version = "MYSQL_5_7"
+#   region           = "northamerica-northeast1"
+#   settings {
+#     tier = "db-f1-micro"
+#   }
+#   deletion_protection = false
 
-}
+# }
 
-resource "google_sql_database" "database" {
-  name     = "my-database"
-  instance = google_sql_database_instance.instance.name
-}
+# resource "google_sql_database" "database" {
+#   name     = "my-database"
+#   instance = google_sql_database_instance.instance.name
+# }
 
-resource "google_sql_user" "user" {
-  name     = "my-user"
-  instance = google_sql_database_instance.instance.name
-  password = "root"
-  host     = "180.191.66.152"
-}
+# resource "google_sql_user" "user" {
+#   name     = "my-user"
+#   instance = google_sql_database_instance.instance.name
+#   password = "root"
+#   host     = "180.191.66.152"
+# }
 
 # resource "google_compute_firewall" "firewall" {
 #   name    = "my-firewall-rule"
